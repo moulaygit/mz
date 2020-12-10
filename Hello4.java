5b80647889ac175a5c5b248f57b96afe51d4ed43
moulaygit <68529397+moulaygit@users.noreply.github.com>
2020-12-09 22:19:58 -0700

    Delete Hello4.java

--- Hello4.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World 4");
-        System.out.println("This file came from the GitHub central repository");
-    }
-}
