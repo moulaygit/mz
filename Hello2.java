03d24313b313d526a4fd524f36821b1ca0e39030
moulaygit <68529397+moulaygit@users.noreply.github.com>
2020-12-09 22:23:12 -0700

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,7 @@
+public class Hello2 {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World 2");
+        System.out.println("This file came from the GitHub central repository");
+    }
+}
---
+++ Hello4.java
@@ -0,0 +1,7 @@
+public class Hello4 {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World 4");
+        System.out.println("This file came from the GitHub central repository");
+    }
+}
