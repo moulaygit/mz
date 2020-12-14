a608482d587ff8433d9fb00b380fa45815d364a5
Moulay Zougari <moulay.zougari@gmail.com>
2020-12-14 12:56:59 -0700

    Added Hello5.java

---
+++ Hello5.java
@@ -0,0 +1,7 @@
+public class Hello5 {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World 5");
+        System.out.println("This file came from the GitHub central repository");
+    }
+}
